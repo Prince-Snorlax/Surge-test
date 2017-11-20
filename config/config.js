a266dcc6ff0c410effa8dc161f1d3df00fa10ae5
Prince-Snorlax <33717215+Prince-Snorlax@users.noreply.github.com>
2017-11-20 20:40:45 +0530

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 'use strict';
 
 // The server port - the port to run Pokemon Showdown under
-exports.port = 8080;
+exports.port = 80;
 
 // The server name (this will speed up the process of renaming some features)
 exports.serverName = "Surge";
